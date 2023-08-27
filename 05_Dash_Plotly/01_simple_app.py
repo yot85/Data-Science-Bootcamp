a706612644d2c33c99ea11ad49b3b35867594c47
yot85 <yot@poczta.fm>
2023-08-27 16:06:10 +0200

    create file

---
+++ 05_Dash_Plotly/01_simple_app.py
@@ -0,0 +1,31 @@
+import dash
+from dash import dcc
+from dash import html
+import plotly.graph_objects as go
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[
+
+    html.H2(children= 'Hello Dash!'),
+
+    dcc.Graph(
+        figure=go.Figure([
+            go.Bar(
+                x=['2017', '2018', '2019'],
+                y=[150, 180, 220],
+                name = 'lokalnie'
+            ),
+            go.Bar(
+                x=['2017', '2018', '2019'],
+                y=[80, 160, 240],
+                name = 'online'
+            )
+        ])
+    )
+
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
